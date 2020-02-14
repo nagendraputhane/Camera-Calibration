3b90b912769563038915838abe3beac0916a067a
nagendra <nagendraputhane@gmail.com>
2020-02-14 14:05:30 +0530

    save corners

--- edge_detection/main.cpp
+++ edge_detection/main.cpp
@@ -2,11 +2,16 @@
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
 #include <iostream>
+#include <fstream>
 
 using namespace cv;
 using namespace std;
 std::vector<cv::Point2f> corners_global;
 int frame_width, frame_height;
+
+bool sorting (const cv::Point p1, const cv::Point p2) {
+    return ((p1.x ) < (p2.x));
+}
 
 class FeatureDetectors
 {
@@ -29,14 +34,47 @@ public:
         for(int i = 0; i < corners_global.size(); i++)
             circle( dst_norm_scaled, corners_global[i], 5,  Scalar(255), 1, LINE_AA, 0 );
 
-        namedWindow( "Corners detected" );
-        imshow( "Corners detected", dst_norm_scaled);
+        //namedWindow( "Corners detected" );
+        //imshow( "Corners detected", dst_norm_scaled);
         cvtColor( dst_norm_scaled, dst_norm_scaled, cv::COLOR_GRAY2BGR );
         video << dst_norm_scaled;
 
         char c=(char)waitKey(1);
         if(c==27)
           exit(0);
+    }
+
+
+
+
+    void corners()
+    {
+        std::sort(corners_global.begin(), corners_global.end(), sorting);
+
+        if(corners_global[0].y < corners_global[1].y)
+        {
+            float x0 = corners_global[0].x;
+            float y0 = corners_global[0].y;
+            float x1 = corners_global[1].x;
+            float y1 = corners_global[1].y;
+            corners_global[0].x = x1;
+            corners_global[0].y = y1;
+            corners_global[1].x = x0;
+            corners_global[1].y = y0;
+        }
+        if(corners_global[2].y < corners_global[3].y)
+        {
+            float x0 = corners_global[2].x;
+            float y0 = corners_global[2].y;
+            float x1 = corners_global[3].x;
+            float y1 = corners_global[3].y;
+            corners_global[2].x = x1;
+            corners_global[2].y = y1;
+            corners_global[3].x = x0;
+            corners_global[3].y = y0;
+        }
+        cout << "Corners are : " << corners_global << endl;
+
     }
 
     void hough(Mat src_gray)
@@ -117,6 +155,9 @@ int main()
 
     VideoWriter video("/home/iq9/nagendra/delay/dataset/output_corners.avi", CV_FOURCC('M','J','P','G'),
                       30, Size(frame_width,frame_height));
+    std::ofstream corner_csv;
+    corner_csv.open ("/home/iq9/nagendra/delay/dataset/corners.csv");
+    //top left, bottom left, top right, bottom right
     while(1)
     {
         Mat frame, frame_bi;
@@ -130,10 +171,18 @@ int main()
         cv::threshold(src_gray, src_gray, 150, 255, ThresholdTypes::THRESH_BINARY);
 
         cv::goodFeaturesToTrack(src_gray, corners_global, 4, 0.01, 2, noArray(), 5,true, 0.04);
-        cout << "Corners are : " << corners_global << endl;
+        //cout << "Corners are : " << corners_global << endl;
 
         fd.harris(src_gray, video);
         //fd.hough(src_gray);
         //fd.rect(src_gray);
+
+        fd.corners();
+        corner_csv << corners_global[0].x << "," << corners_global[0].y << "," <<
+                      corners_global[1].x << "," << corners_global[1].y << "," <<
+                      corners_global[2].x << "," << corners_global[2].y << "," <<
+                      corners_global[3].x << "," << corners_global[3].y << "\n";
+
     }
+    corner_csv.close();
 }
