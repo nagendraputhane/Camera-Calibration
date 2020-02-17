b574600c8c5ad0b1a53209b538549135b4df0c4e
nagendra <nagendraputhane@gmail.com>
2020-02-17 15:10:05 +0530

    detect mid points on edge

--- edge_detection/main.cpp
+++ edge_detection/main.cpp
@@ -7,7 +7,9 @@
 using namespace cv;
 using namespace std;
 std::vector<cv::Point2f> corners_global;
-int frame_width, frame_height;
+int frame_width, frame_height, val = 0;
+int first_black_row_mid, first_black_col_mid;
+int second_black_row_mid, second_black_col_mid;
 
 bool sorting (const cv::Point p1, const cv::Point p2) {
     return ((p1.x ) < (p2.x));
@@ -16,8 +18,10 @@ bool sorting (const cv::Point p1, const cv::Point p2) {
 class FeatureDetectors
 {
 public:
-    void harris(Mat src_gray, VideoWriter video)
+    void harris(Mat src_gray)
     {
+        val ++;
+        val = val % 100;
         int blockSize = 5, apertureSize = 3, thresh = 130; //2, 75
         double k = 0.04;
 
@@ -34,14 +38,64 @@ public:
         for(int i = 0; i < corners_global.size(); i++)
             circle( dst_norm_scaled, corners_global[i], 5,  Scalar(255), 1, LINE_AA, 0 );
 
-        //namedWindow( "Corners detected" );
-        //imshow( "Corners detected", dst_norm_scaled);
-        cvtColor( dst_norm_scaled, dst_norm_scaled, cv::COLOR_GRAY2BGR );
-        video << dst_norm_scaled;
+        cout << src_gray.at<int>(0, 0);
 
-        char c=(char)waitKey(1);
-        if(c==27)
-          exit(0);
+        /*namedWindow( "Corners detected" );
+        imshow( "Corners detected", src_gray);
+        cvtColor( dst_norm_scaled, dst_norm_scaled, cv::COLOR_GRAY2BGR );
+        //video << dst_norm_scaled;*/
+    }
+
+    void edge_points(Mat src_gray)
+    {
+        int end_loop = 0;
+        for (int i = 0; i < 720 && end_loop != 1; i++)
+        {
+            for (int j = 0; j < 1280 && end_loop != 1; j++)
+            {
+                if (static_cast<int>(cv::norm(src_gray.row(i).col(j))) == 0)
+                {
+                    int first_black_r = i;
+                    int first_black_c = j;
+                    if (first_black_c > 300)
+                    {
+                        continue;
+                    }
+                    cout << "first_black_r : " << first_black_r << ", first_black_c : " << first_black_c << endl;
+                    int row_to_track = first_black_r + 50;
+                    cout << "row_to_track : " << row_to_track << endl;
+                    int column_to_track;
+                    for ( int coli = first_black_c; coli < 1280; coli++)
+                    {
+                        if (static_cast<int>(cv::norm(src_gray.row(row_to_track).col(coli))) == 255)
+                        {
+                            int last_black_c = coli - 1;
+                            cout << "last_black_c : " << last_black_c << endl;
+                            column_to_track = (first_black_c + last_black_c)/2;
+                            cout << "column_to_track : " << column_to_track << endl;
+                            break;
+                        }
+                    }
+                    for ( int k = 0; k < 720; k++)
+                    {
+                        if (static_cast<int>(cv::norm(src_gray.row(k).col(column_to_track))) == 0)
+                        {
+                            first_black_row_mid = k;
+                            second_black_row_mid = row_to_track;
+                            cout << "first_black_row_mid : " << first_black_row_mid << endl;
+                            first_black_col_mid = column_to_track;
+                            second_black_col_mid = first_black_c;
+                            cout << "Point to track : " << first_black_row_mid << ", " << first_black_col_mid << endl;
+                            cout << "Point to track : " << second_black_row_mid << ", " << second_black_col_mid << endl;
+                            cout << "------------------------" << endl;
+                            end_loop = 1;
+                            //exit(0);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
     }
 
 
@@ -73,7 +127,7 @@ public:
             corners_global[3].x = x0;
             corners_global[3].y = y0;
         }
-        cout << "Corners are : " << corners_global << endl;
+        //cout << "Corners are : " << corners_global << endl;
 
     }
 
@@ -99,9 +153,7 @@ public:
             namedWindow( "Edges detected" );
             imshow( "Edges detected", src_gray_c );
 
-            char c=(char)waitKey(25);
-            if(c==27)
-                exit(0);
+
         }
     }
 
@@ -114,6 +166,11 @@ public:
 
         vector<vector<Point> > contours;
         findContours( canny_output, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE );
+
+        for (int i =0; i < contours.size(); i++)
+        {
+            //cout << "points" << contours.at(i);
+        }
 
         vector<vector<Point> > contours_poly( contours.size() );
         vector<Rect> boundRect( contours.size() );
@@ -133,12 +190,12 @@ public:
             drawContours( drawing, contours_poly, (int)i, color );
             rectangle( drawing, boundRect[i].tl(), boundRect[i].br(), color, 1 );
         }
-        namedWindow( "Contours" );
-        imshow( "Contours", drawing );
+        //namedWindow( "Contours" );
+        //imshow( "Contours", drawing );
 
-        char c=(char)waitKey(25);
-        if(c==27)
-            exit(0);
+        //char c=(char)waitKey(25);
+        //if(c==27)
+            //exit(0);
     }
 };
 
@@ -153,10 +210,12 @@ int main()
     frame_width = cap.get(CV_CAP_PROP_FRAME_WIDTH);
     frame_height = cap.get(CV_CAP_PROP_FRAME_HEIGHT);
 
-    VideoWriter video("/home/iq9/nagendra/delay/dataset/output_corners.avi", CV_FOURCC('M','J','P','G'),
-                      30, Size(frame_width,frame_height));
+    //VideoWriter video("/home/iq9/nagendra/delay/dataset/output_corners.avi", CV_FOURCC('M','J','P','G'),
+    //                  30, Size(frame_width,frame_height));
     std::ofstream corner_csv;
-    corner_csv.open ("/home/iq9/nagendra/delay/dataset/corners.csv");
+    corner_csv.open ("/home/iq9/nagendra/delay/corners.csv");
+    std::ofstream points_to_track;
+    points_to_track.open ("/home/iq9/nagendra/delay/points_to_track.csv");
     //top left, bottom left, top right, bottom right
     while(1)
     {
@@ -171,9 +230,8 @@ int main()
         cv::threshold(src_gray, src_gray, 150, 255, ThresholdTypes::THRESH_BINARY);
 
         cv::goodFeaturesToTrack(src_gray, corners_global, 4, 0.01, 2, noArray(), 5,true, 0.04);
-        //cout << "Corners are : " << corners_global << endl;
 
-        fd.harris(src_gray, video);
+        fd.harris(src_gray);
         //fd.hough(src_gray);
         //fd.rect(src_gray);
 
@@ -182,7 +240,16 @@ int main()
                       corners_global[1].x << "," << corners_global[1].y << "," <<
                       corners_global[2].x << "," << corners_global[2].y << "," <<
                       corners_global[3].x << "," << corners_global[3].y << "\n";
+        fd.edge_points(src_gray);
 
+        points_to_track << first_black_row_mid << "," << first_black_col_mid << "," <<
+                           second_black_row_mid << "," << second_black_col_mid << "\n";
+
+        first_black_row_mid = first_black_col_mid = second_black_row_mid = second_black_col_mid = 0;
+        char c=(char)waitKey(25);
+        if(c==27)
+            break;
     }
     corner_csv.close();
+    points_to_track.close();
 }
